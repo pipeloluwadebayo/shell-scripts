8ddbb9410ed6c83c4d2647cfe7cd8c410db38eec
pipeloluwadebayo <pipeloluwapraise@gmail.com>
2023-05-08 13:50:47 -0400

    user creation shell script

---
+++ useraccounts.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# This script creates a user on the local system
+# Requirements:
+# -username
+# -full name
+# -password
+# The username, password and host for the account will be displayes
+# The user will be prompted to change their password on first login
+
+# Ensure script is executed with superuser privileges
+
+if [[ ${UID} -ne 0 ]]
+then 
+	echo "Please run as user or as root"
+	exit 1
+fi
+
+# Prompt for username
+read -p 'Enter your username: '  USER_NAME
+
+# Prompt for real name
+read -p 'Enter your fullname: ' FULL_NAME
+
+# Prompt for password
+read -p 'Enter your password: ' PASSWORD
+
+echo 'You will be prompted to set a new password on your first login'
+
+# Create account
+useradd -c "${FULL_NAME}" -m ${USER_NAME}
+
+# Check if useradd command succedded
+if [[ "${?}" -ne 0 ]]
+then
+	echo 'The account could not be created.'
+	exit 1
+fi
+
+# Set the password
+echo ${PASSWORD} | passwd --stdin ${USERNAME}
+
+# Check if passwd command succeeded
+if [[ "${?}" -ne 0 ]]
+then
+	echo 'The password could not be set'
+	exit 1
+fi
+
+# Force password change on first login
+passwd -e ${USER_NAME}
+
+chage -M 30 ${USER_NAME}
+
+chage -W 7 ${USER_NAME}
+
+# Display user information
+
+echo 
+echo 'Username:'
+echo "${USER_NAME}"
+echo
+echo 'Fullname:'
+echo "${FULL_NAME}"
+echo
+echo 'Password:'
+echo "${PASSWORD}"
+echo
+echo 'Host:'
+echo "${HOSTNAME}"
+echo
+echo 'Your password will expire in 30 days and you will need to set a new password'
+echo
+echo 'You will receive a warning 7 days before your password expires'
+echo
+chage -l ${USER_NAME}
+exit 0
